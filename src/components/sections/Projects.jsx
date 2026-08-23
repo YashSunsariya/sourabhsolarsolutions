ecc966d4c0a7e2b1e16e142ceca184909eb204df
Yash Sunsariya <yashsunsariya2005@gmail.com>
2026-08-23 23:41:03 +0530

    updated files

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -130,12 +130,6 @@ function Projects() {
                       </div>
                     </dl>
                     <p className="case-details">{project.details}</p>
-                    <a href="#contact" className="case-link">
-                      View Project
-                      <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-                        <path d="M5 12h14M13 6l6 6-6 6" />
-                      </svg>
-                    </a>
                   </div>
                 </article>
               </Reveal>
